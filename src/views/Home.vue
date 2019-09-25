5ddf901376ad5af31889b2a04767ab9ae318b1fe
Andres David P <andru2400@hotmail.com>
2019-09-25 11:57:06 -0500

    Primer commit

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,43 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  
+ <ion-card>
+   
+    <ion-item>
+      <ion-label position="floating">Escriba nombre: </ion-label>
+      <ion-input :value="greeting" @input="greeting = $event.target.value"></ion-input>
+    </ion-item>
+        <ion-button expand="full" color="secondary" v-on:click="oneClick" href="#">Enviar</ion-button>        
+  
+  </ion-card>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
-  name: 'home',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      greeting: ""
+    };
+  },
+  methods: {
+    // oneClick() {
+    //   this.$ionic.alertController
+    //     .create({
+    //       header: "Alerta",
+    //       subHeader: "Sub Alerta",
+    //       message: "Hola mundo",
+    //       buttons: ["ok"]
+    //     })
+    //     .then(a => a.present());
+    // }
+    async oneClick() {
+      const alert = await this.$ionic.alertController.create({
+        header: "Alerta",
+        subHeader: "Sub Alerta",
+        message: this.greeting,
+        buttons: ["ok"]
+      });
+      await alert.present();
+    }
   }
-}
+};
 </script>
