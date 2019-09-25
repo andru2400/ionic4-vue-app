d5dc0e04e466153ca8d8033537162f827a0260f9
Andres David P <andru2400@hotmail.com>
2019-09-25 16:30:02 -0500

    Me quede en el minito 53 10

---
+++ src/views/Detail.vue
@@ -0,0 +1,30 @@
+<template>
+    <ion-card>
+      <ion-card-header>
+        <ion-label>
+            {{props.post.title}}
+        </ion-label>
+        <ion-card-content>
+
+        </ion-card-content>
+      </ion-card-header>
+    </ion-card>
+</template>
+
+
+<script>
+export default {
+  props: ["params"],
+  data() {
+    return {
+      image: "",
+      props: []
+    };
+  },
+  mounted() {
+    // console.log(this.props);
+    this.props = this.$route.params;
+    // console.log(this);
+  }
+};
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,42 +1,39 @@
 <template>
-  
- <ion-card>
-   
-    <ion-item>
-      <ion-label position="floating">Escriba nombre: </ion-label>
-      <ion-input :value="greeting" @input="greeting = $event.target.value"></ion-input>
-    </ion-item>
-        <ion-button expand="full" color="secondary" v-on:click="oneClick" href="#">Enviar</ion-button>        
-  
-  </ion-card>
+  <div>
+    <ion-card v-for="post in posts" :key="post.id" style="border: 1px solid blue;">
+      <ion-card-content>
+        <template v-if="post.data.thumbnail.startsWith('https://')">
+          <img :src="post.data.thumbnail">
+        </template>
+        <ion-label color="light">
+          {{post.data.title}}
+        </ion-label>
+        <ion-button color="tertiary" expand="full" @click="viewMore(post.data)">
+          View More
+        </ion-button>
+      </ion-card-content>
+    </ion-card>
+  </div>
 </template>
 
 <script>
+import axios from "axios";
 export default {
   data() {
     return {
-      greeting: ""
+      posts: []
     };
   },
+  async mounted() {
+    const response = await axios.get(
+      "https://www.reddit.com/r/nextfuckinglevel.json"
+    );
+    this.posts = response.data.data.children;
+  },
   methods: {
-    // oneClick() {
-    //   this.$ionic.alertController
-    //     .create({
-    //       header: "Alerta",
-    //       subHeader: "Sub Alerta",
-    //       message: "Hola mundo",
-    //       buttons: ["ok"]
-    //     })
-    //     .then(a => a.present());
-    // }
-    async oneClick() {
-      const alert = await this.$ionic.alertController.create({
-        header: "Alerta",
-        subHeader: "Sub Alerta",
-        message: this.greeting,
-        buttons: ["ok"]
-      });
-      await alert.present();
+    viewMore(post) {
+      // console.log(post);
+      this.$router.push({ name: "detail", params: { post } });
     }
   }
 };
